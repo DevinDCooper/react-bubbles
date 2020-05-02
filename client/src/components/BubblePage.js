ee7bc385e4f3ba097d1b3a8b50b1fab064d1addc
Devin Cooper <37602228+DevinDCooper@users.noreply.github.com>
2020-05-01 21:01:35 -0700

    MVP

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,20 +1,26 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 
+
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
-
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
-  // fetch your colors data from the server when the component mounts
-  // set that data to the colorList state property
-
+    useEffect (() => {
+      axiosWithAuth()
+        .get('/colors')
+        .then(res => {
+          setColorList(res.data)
+        })
+        .catch(err => {
+          console.log("Error fetching colors", err.response)
+        })
+      },[])
   return (
     <>
       <ColorList colors={colorList} updateColors={setColorList} />
       <Bubbles colors={colorList} />
     </>
   );
-};
-
+  };
 export default BubblePage;
--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
-import axios from "axios";
+
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+
+
 
 const initialColor = {
   color: "",
   code: { hex: "" }
 };
-
 const ColorList = ({ colors, updateColors }) => {
   console.log(colors);
+  
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
 
@@ -18,13 +21,29 @@ const ColorList = ({ colors, updateColors }) => {
 
   const saveEdit = e => {
     e.preventDefault();
-    // Make a put request to save your updated color
-    // think about where will you get the id from...
-    // where is is saved right now?
+    console.log(colorToEdit);
+    axiosWithAuth()
+    .put(`/colors/${colorToEdit.id}`, colorToEdit)
+    .then(res => {
+      console.log(res.data);
+      updateColors([...colors, res.data]);
+      setEditing(false);
+      
+    })
+    .catch(err =>
+      console.log("Error saving color", err));
   };
-
+  
   const deleteColor = color => {
-    // make a delete request to delete this color
+    axiosWithAuth()
+    .delete(`/colors/${color.id}`)
+    .then(res => {
+      console.log(res.data);
+      updateColors([...colors]);
+      window.location.reload();
+    })
+    .catch(err =>
+      console.log("Error deleting color", err));
   };
 
   return (
@@ -76,6 +95,7 @@ const ColorList = ({ colors, updateColors }) => {
           </label>
           <div className="button-row">
             <button type="submit">save</button>
+            
             <button onClick={() => setEditing(false)}>cancel</button>
           </div>
         </form>
@@ -85,5 +105,4 @@ const ColorList = ({ colors, updateColors }) => {
     </div>
   );
 };
-
 export default ColorList;
